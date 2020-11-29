e675a5edb2a007b46a6d2e4cd2c010454b39593e
biv147 <37488403+biv147@users.noreply.github.com>
2020-11-29 18:10:21 -0500

    added files

---
+++ app.py
@@ -0,0 +1,64 @@
+import json
+import paho.mqtt.client as mqtt
+from flask import Flask, request, render_template, jsonify, redirect
+import sqlite3 as sql
+
+
+
+#flask name
+app = Flask(__name__)
+
+username = ""
+password = ""
+status = ""
+
+#callback function to retrieve message
+def on_message(client, userdata, message):
+    print("Data recieved: "+ str(message.payload.decode("utf-8")))
+    print("message topic=", message.topic)
+
+
+#callback function to connect to the broker
+def on_connect(client, userdata, flags, rc):
+    print("connected ok!")
+    client.publish("Data","This is a test")
+    #client.subscribe("LockState")
+    #client.subscribe("Data")
+
+
+#creating the client and connecting it
+client = mqtt.Client()
+client.on_message = on_message
+print("connecting to broker")
+client.connect("localhost", 1833, 60)  # connect to broker
+
+#subsribing and getting the messages
+client.on_connect = on_connect
+client.on_message = on_message
+
+#starting the loop
+client.loop_start()
+
+#endpoints
+@app.route("/", methods=["GET","POST"])
+def hello_world():
+    if request.method == "POST":
+        req = request.form
+
+        username = req.get("user")
+        password = req.get("pass")
+
+        print(username)
+        print(password)
+
+        client.publish("Data", username)
+
+        return redirect(request.url)
+
+    return render_template('home.html')
+
+
+#running flask
+if __name__ == '__main__':
+    app.run()
+
---
+++ lock.py
@@ -0,0 +1,46 @@
+import paho.mqtt.client as mqtt
+import time
+
+lock_status = "nothing"
+
+#callback function to retrieve message
+def on_message(client, userdata, message):
+    print("Data recieved: "+ str(message.payload.decode("utf-8")))
+    #print("message topic=", message.topic)
+    #chageStatus(client, str(message.payload.decode("utf-8")))
+
+#callback function to connect to the broker
+def on_connect(client, userdata, flags, rc):
+    print("connected ok!")
+    client.subscribe("Data")
+
+
+def chageStatus(client, status):
+    global lock_status
+    lock_status = status
+    #print(lock_status)
+    #client.publish("LockState", lock_status)
+
+
+def main():
+    # creating the client and connecting the client
+    client = mqtt.Client()
+    client.connect("localhost", 1833, 60)
+    print("lock connected to broker")
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    while True:
+        # val = input("Enter what you want to send: ")
+        # val = "hello"
+        #global lock_status
+        #print(lock_status)
+        #client.publish("LockState", lock_status)
+
+        time.sleep(5)
+
+    client.loop_forever()
+
+if __name__ == '__main__':
+    main()
